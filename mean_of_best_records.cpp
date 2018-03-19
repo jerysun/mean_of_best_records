4c5e3f4eeb2d98a2b3cd76e1ec3e5a0778a0523a
Jerry Sun <32456849+jerysun@users.noreply.github.com>
2018-03-19 11:15:38 +0100

    Typos correction in mean_of_best_records.cpp

--- mean_of_best_records.cpp
+++ mean_of_best_records.cpp
@@ -1,5 +1,5 @@
 /*
- * filename: mean_of_best_records.h
+ * filename: mean_of_best_records.cpp
  * describe: Arithmetic mean problem of the Top N records in Database SQL.
  *           There are a list of athletes' records consisting of (id, ath_id,
  *           name, record, r_date), a single athlete may have several
